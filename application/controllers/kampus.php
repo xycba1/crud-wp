2cee14aef6a0982c7b0d9640dd7c4efb9cae7371
inudwijaya@gmail.com <inudwijaya@gmail.com>
2023-10-10 03:18:27 -0700

    Tugas Pertemuan 4

---
+++ application/controllers/kampus.php
@@ -0,0 +1,70 @@
+<?php
+class kampus extends CI_Controller{
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('m_data');
+        $this->load->helper('url');
+    }
+
+    function index (){
+        $data['mahasiswa'] = $this->m_data->tampil_data()->result();
+        $this->load->view('tampil_data', $data);
+    }
+
+    function tambah(){
+        $this->load->view('input_data');
+    }
+
+    function tambah_aksi(){
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $alamat = $this->input->post('alamat');
+        $pekerjaan = $this->input->post('pekerjaan');
+
+        $data = array(
+            'nim' => $nim,
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'pekerjaan' => $pekerjaan
+        );
+
+        $this->m_data->input_data($data, 'mahasiswa');
+        redirect('kampus/index');
+    }
+
+    function edit($id){
+        $where = array('id' => $id);
+        $data['mahasiswa'] = $this->m_data->edit_data($where, 'mahasiswa')->result();
+        $this->load->view('edit_data', $data);
+    }
+
+    function update(){
+        $id = $this->input->post('id');
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $alamat = $this->input->post('alamat');
+        $pekerjaan = $this->input->post('pekerjaan');
+
+        $data = array(
+            'nim' => $nim,
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'pekerjaan' => $pekerjaan
+        );
+
+        $where = array(
+            'id' => $id
+        );
+
+        $this->m_data->update_data($where, $data, 'mahasiswa');
+        redirect('kampus');
+    }
+	
+	
+	function hapus($id){
+		$where = array('id' => $id);
+		$this->m_data->hapus_data($where,'mahasiswa');
+		redirect('kampus');
+	}
+}
---
+++ application/views/edit_data.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Data</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>desain/css/stylesheet.css">
+</head>
+<body>
+    <center>
+        <h1>Edit Data Mahasiswa</h1>
+    </center>
+    <?php foreach ($mahasiswa as $u) { ?>
+    <form action="<?php echo base_url() . 'kampus/update'; ?>" method="post" enctype="multipart/form-data">
+        <table style="margin: 20px auto;">
+            <tr>
+                <td>NIM</td>
+                <td>
+                    <input type="hidden" name="id" value="<?php echo $u->id; ?>">
+                    <input type="text" name="nim" value="<?php echo $u->nim; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>
+                    <input type="text" name="nama" value="<?php echo $u->nama; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>
+                    <input type="text" name="alamat" value="<?php echo $u->alamat; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td>
+                <td>
+                    <input type="text" name="pekerjaan" value="<?php echo $u->pekerjaan; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" name="submit" value="Update">
+                    <?php echo anchor('kampus', '<input type="button" value="Kembali">'); ?>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <?php } ?>
+</body>
+</html>
+
